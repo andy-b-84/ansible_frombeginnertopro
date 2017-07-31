ef68938d96a8c801b9d0357f6084508d21060c8a
Patrick Laxton <patrick.laxton@protonmail.com>
2017-07-31 16:00:32 +0200

    chapter 6 modules well that works only if run as root, and I don't want to change my system clock every other minute :)

--- chapter6_modules/from_site/timetest.py
+++ chapter6_modules/from_site/timetest.py
@@ -1,9 +1,37 @@
 #!/usr/bin/python
 
 import datetime
+import sys
 import json
+import os
+import shlex
 
+args_file = sys.argv[1]
+args_data = file(args_file).read()
+
+arguments = shlex.split(args_data)
+
+for arg in arguments:
+    if "=" in arg:
+        (key, value) = arg.split("=")
+        if key == "time":
+            rc = os.system("date -s \"%s\"" % value)
+            if rc != 0:
+                print(json.dumps({
+                    "failed" : True,
+                    "msg" : "failed setting the time"
+                }))
+                sys.exit(1)
+
+            date = str(datetime.datetime.now())
+            print(json.dumps({
+                "time": date,
+                "changed": True
+            }))
+            sys.exit(0)
+                 
 date = str(datetime.datetime.now())
 print(json.dumps({
-    "time": date
+    "time": date,
+    "changed": True
 }))
